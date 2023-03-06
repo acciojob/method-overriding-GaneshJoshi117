69e118e43e65f02ac3a5c9fc9881f84f571a2177
GaneshJoshi117 <ganeshjoshi583@gmail.com>
2023-03-06 20:31:52 +0530

    debugging

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        A obj1 = new A();
+        obj1.meth();
         B obj2 = new B();
         obj2.meth();
     }
