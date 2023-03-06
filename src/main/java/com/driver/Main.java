2011286570657f7a8e7dd2b5862ab0db3600f988
GaneshJoshi117 <ganeshjoshi583@gmail.com>
2023-03-06 20:05:16 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        A obj1 = new B();
+        obj1.meth();
+        B obj2 = new B();
+        obj2.meth();
+    }
 }
